a7d6df56ce1cab2f678d0dc2067dd53e83b1cb6b
Masud-Ndatsu <masudndatsu@gmail.com>
2025-06-29 01:53:49 +0100

    fix: added prisma service to opportunity module

--- src/opportunities/opportunities.module.ts
+++ src/opportunities/opportunities.module.ts
@@ -3,9 +3,10 @@ import { OpportunitiesService } from './opportunities.service';
 import { OpportunitiesController } from './opportunities.controller';
 import { CategoriesService } from './categories/categories.service';
 import { CategoriesController } from './categories/categories.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [OpportunitiesService, CategoriesService],
-  controllers: [OpportunitiesController, CategoriesController]
+  providers: [OpportunitiesService, CategoriesService, PrismaService],
+  controllers: [OpportunitiesController, CategoriesController],
 })
 export class OpportunitiesModule {}
